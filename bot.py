1d18bfcdb7398cd24804fa6a2ffc067729d16e24
atmay <atmay@yandex.ru>
2022-07-08 01:11:42 +0300

    code refactored

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.utils.markdown import text, italic, code
+from aiogram.utils.markdown import text
 from aiogram.utils.emoji import emojize
 from aiogram.types import ParseMode
 
 from settings import BOT_TOKEN
+from messages import get_contacts_msg, get_help_msg, get_start_msg
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -19,36 +20,49 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` command
     """
-    msg = text(emojize("🌿 Шалом!\nДобро пожаловать в Акко \n "
-                       "Этот бот поможет получить ответы на частые вопросы о городе \n"
-                       "Вот список доступных команд:\n"
-                       "\n"
-                       "/help - просмотреть список команд\n"
-                       "🌿 Легкой абсорбции!"))
+    msg = get_start_msg()
     await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
 
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    msg = text(emojize("🌴 Вот список доступных команд:\n\n"
-                       "/myvisit - как записаться в МВД через приложение MyVisit (в картинках!)\n"
-                       "/transport - такси, РавКав и приложение Moovit\n"
-                       "/shops - магазины в Акко и окрестностях\n"
-                       "\nНе нашел нужной команды, которую стоит добавить? "
-                       "Напиши @atmayatmay об этом - давайте улучшать ботика вместе!"))
+    msg = get_help_msg()
     await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
 
 
 @dp.message_handler(commands=['myvisit'])
-async def process_help_command(message: types.Message):
+async def process_myvisit_command(message: types.Message):
     msg = text(emojize("Выбери, какую запись ты хочешь сделать:\n"
                        ))
     # TODO add buttons for Darkon and Teudat Zeut
     await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
 
 
+@dp.message_handler(commands=['contacts'])
+async def process_contacts_command(message: types.Message):
+    msg = get_contacts_msg()
+    await message.reply(msg, parse_mode=ParseMode.HTML)
+
+
+@dp.message_handler(commands=['transport'])
+async def process_transport_command(message: types.Message):
+    msg = text(emojize("Кучер, запрягай! 🦄\n"
+                       "О чем тебе рассказать?\n"
+                       ))
+    # TODO add buttons for Ravkav, Moovit, taxis
+    await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
+
+
+@dp.message_handler(commands=['shops'])
+async def process_shops_command(message: types.Message):
+    msg = text(emojize("Что будем покупать?\n"
+                       ))
+    # TODO add buttons for grocery stores, electronics, home utensils
+    await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
+
+
 @dp.message_handler()
-async def echo(message: types.Message):
+async def handle_random_input(message: types.Message):
     """handle any user message"""
     msg = text(emojize('Я не очень умный бот, мне это непонятно 🥺\n'
                        'Жми /help и увидишь список доступных команд \n'))
